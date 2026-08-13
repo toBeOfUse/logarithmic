bcbc609c7058bf6f9123cfdfd27703e83dfb7405
Mitch J <mjacovet@kent.edu>
2026-08-13 00:05:33 -0400

    try out "vazirmatn" as main font

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -25,7 +25,7 @@ export const links = () => [
   { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700&family=Lora:ital@0;1&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;500;600;700&family=Lora:ital@0;1&display=swap",
   },
 ];
 
